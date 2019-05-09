af191c6a18d1d7a659c13728893ed532b2a923b4
Jayaprakash sivagami <jayaprakashchinnaraji2003@gmail.com>
2019-05-09 15:26:20 +0530

    text added successfully

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const http = require('http');
 const fs = require('file-system');
 
-http.createServer(function(req,res){
-	try{
-		let file = fs.readFileSync('.' + req['url'] + '.txt', 'utf8');
-		res.write(file);
-		}catch(err){
-			if(err){
-				res.write("File Not found");
-			}
-		}res.end();
-}).listen(12345);
+let file = fs.readFileSync('./pwd.txt', 'utf8');
+fs.writeFileSync('./pwd.txt', file + 'new text', 'utf8');
 
